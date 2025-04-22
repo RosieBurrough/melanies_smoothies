97402418262354369eb6ba6f803e0e948f211082
Rosie <roseanna.burrough@mcicoach.com>
2025-04-22 13:43:25 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,7 @@ pd_df = my_dataframe.to_pandas()
 ingredients_list = st.multiselect(
         'Choose up to 5 Ingredients'
     , my_dataframe
-    , max_selections=5
+    , max_selections=6
 )
 if ingredients_list:
     ingredients_string = ''
